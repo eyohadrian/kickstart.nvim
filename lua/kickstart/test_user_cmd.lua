df80164509b01e444eb66b2b10bd0ae164481f96
Adrián Asensio <adrianasensio@Adrians-MacBook-Pro.local>
2025-12-29 12:45:15 +0100

    Added custom command to a key mapping

--- lua/kickstart/test_user_cmd.lua
+++ lua/kickstart/test_user_cmd.lua
@@ -2,3 +2,8 @@ cmd = vim.api.nvim_create_user_command
 
 
 cmd('TestNewCommand', function() print("TestWorked") end, {})
+
+
+local map = vim.keymap.set
+
+map("n", "<leader>z", "<cmd>TestNewCommand<CR>", { desc = "CMD enter command mode" })
